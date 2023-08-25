aa0450d1b7798ce5ff6b980b36b88883d7ef9389
eeunha <eunha.dev@gmail.com>
2023-08-25 17:34:56 +0900

    Init

---
+++ ex04_operator.sql
@@ -0,0 +1,33 @@
+-- ex04_operator.sql
+
+/*
+    연산자, Operator
+    
+    1. 산술 연산자
+    - +, -, *, /
+    - %(없음) > 함수로 제공(mod())
+    
+    2. 
+
+
+*/
+
+select 
+    population,
+    area,
+    population + area 
+from 
+    tblCountry;
+
+
+
+
+
+
+
+
+
+
+
+
+
